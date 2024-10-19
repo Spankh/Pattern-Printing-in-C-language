a45cbfddeb18652f669cc027c9fcefb283bff705
PANKHURI-3110 <110279557+Spankh@users.noreply.github.com>
2024-10-20 00:24:53 +0530

    Even Number Triangle

---
+++ Even_Number_Triangle.c
@@ -0,0 +1,35 @@
+// Even Number Triangle 
+#include <stdio.h>
+
+int main()
+{
+    int n; // Number of lines
+    printf("Enter no of lines: ");
+    scanf(“%d”, &n); 
+    
+    for (int i = 1; i <= n; i++){ //outer loop for row
+          int a = 0;
+        for (int j = 1; j <= i; j++) { // inner loop for col
+          a = a + 2; // condition to find next even no in series a initialized by 0
+            printf("%d",a);
+              
+        }
+        printf("\n");
+     
+    }
+    return 0;
+}
+
+/*
+=== Code Execution Successful ===
+
+Enter no of lines: 5
+2
+24
+246
+2468
+246810
+
+
+=== Code Execution Successful ===
+*/
