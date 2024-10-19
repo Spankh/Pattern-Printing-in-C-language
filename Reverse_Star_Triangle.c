90a449635b4138fbae800842a4f5afb1d719e0e6
PANKHURI-3110 <110279557+Spankh@users.noreply.github.com>
2024-10-19 23:04:38 +0530

    Reverse Star Triangle - by using row col realtion in each line (i+j = n+1)

---
+++ Reverse_Star_Triangle.c
@@ -0,0 +1,36 @@
+// Reverse Star Triangle - by using row col realtion in each line (i+j = n+1) 
+
+#include <stdio.h>
+
+int main()
+{
+    int n; // Number of lines
+    printf("Enter no of lines: ");
+    scanf(“%d”, &n); 
+    
+    for (int i = 1; i <= n; i++){ //outer loop for row
+        
+        for (int j = 1; j <= n + 1 - i; j++) { // inner loop for col
+         
+            printf("*");
+            
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
+
+
+
+=== Code Execution Successful ===
+
+Enter no of lines: 5
+*****
+****
+***
+**
+*
+
+
+=== Code Execution Successful ===
