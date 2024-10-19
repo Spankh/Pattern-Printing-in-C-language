db3e4c073e6908468dbc3a0215e4685a1bf0e51a
PANKHURI-3110 <110279557+Spankh@users.noreply.github.com>
2024-10-19 22:47:40 +0530

    Number Pattern - Where row number printed

---
+++ Number_Row_Pattern.c
@@ -0,0 +1,35 @@
+// Number Pattern - Where row number printed
+
+#include <stdio.h>
+
+int main()
+{
+    int n; // Number of lines
+    printf("Enter no of lines: ");
+    scanf(“%d”, &n); 
+    
+    for (int i = 1; i <= n; i++){ //outer loop for row
+        
+        for (int j = 1; j <= n; j++) { // inner loop for col
+         
+            printf("%d", i);
+            
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
+
+
+=== Code Execution Successful ===
+
+Enter no of lines: 5
+11111
+22222
+33333
+44444
+55555
+
+
+=== Code Execution Successful ===
