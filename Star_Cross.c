1cda0faec37ec3591cb82e43dabee9cfb4bea323
PANKHURI-3110 <110279557+Spankh@users.noreply.github.com>
2024-10-20 01:49:44 +0530

    Star Cross

--- Star_Cross.c
+++ Star_Cross.c
@@ -7,7 +7,8 @@ int main()
     int n; 
     printf("Enter No of Rows: ");
     scanf(“%d”,&n); 
-    
+    if (n%2 != 0) // to check n value is odd or not
+    {
     for (int i = 1; i <= n; i++){ //outer loop for row
         
         for (int j = 1; j <= n; j++) { // inner loop for col
@@ -22,8 +23,28 @@ int main()
         printf("\n");
         
     }
+    }
+    else 
+    printf("we need odd no to print cross");
     return 0;
 }
+
+
+/*
+=== Code Execution Successful ===
+
+Enter No of Rows: 7
+*     *
+ *   * 
+  * *  
+   *   
+  * *  
+ *   * 
+*     *
+
+
+=== Code Execution Successful ===
+*/
 
 
 /*
