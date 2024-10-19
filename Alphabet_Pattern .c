b0065dab3998f0a9dca6819ec376ef7580a0f1f2
PANKHURI-3110 <110279557+Spankh@users.noreply.github.com>
2024-10-20 00:48:06 +0530

    Alphabet Pattern - by using its ASCII value

---
+++ Alphabet_Pattern .c
@@ -0,0 +1,35 @@
+// Alphabet Pattern - by using its ASCII value
+#include <stdio.h>
+
+int main()
+{
+    int n; // Number of lines
+    printf("Enter no of lines: ");
+    scanf(“%d”, &n); 
+   
+    for (int i = 1; i <= n; i++){ //outer loop for row
+          int a = 1;
+        for (int j = 1; j <= n; j++) { // inner loop for col
+         int d = a + 64; // condition to find 'A' ASCII value is 65 and 'a' ASCII value is 97
+          char ch = (char) d;
+            printf("%c",ch);
+              a++;
+        }
+        printf("\n");
+     
+    }
+    return 0;
+}
+
+/*
+=== Code Execution Successful ===
+
+Enter no of lines: 4
+ABCD
+ABCD
+ABCD
+ABCD
+
+
+=== Code Execution Successful ===
+*/
