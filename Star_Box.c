c790712f83e6fb713b75de4a835addcf64176c06
PANKHURI-3110 <110279557+Spankh@users.noreply.github.com>
2024-10-19 21:36:25 +0530

    Star Box - where no of row & col are same

---
+++ Star_Box.c
@@ -0,0 +1,22 @@
+// Star Box - where no of row and col are same
+
+#include <stdio.h>
+
+int main()
+{
+    int n; 
+    scanf(“%d”,&n); 
+    
+    for (int i = 1; i <= n; i++){ //outer loop for row
+        
+        for (int j = 1; j <= n; j++) { // inner loop for col
+         
+            printf("* " );
+            
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
+
