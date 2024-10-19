e189aaec22b8256ef909d41b089e059114d09f74
PANKHURI-3110 <110279557+Spankh@users.noreply.github.com>
2024-10-20 02:51:17 +0530

    Even Number Series - continue no series with n set of no in a row

--- Even_Number_Series.c
+++ Even_Number_Series.c
@@ -15,6 +15,7 @@ int main()
         for (int j = 1; j <= n; j++) { // inner loop for col
             a = a + 2; // condition to find next even no in series start with 0
             printf("%d",a);
+             printf(" ");
             
         }
         printf("\n");
@@ -27,11 +28,12 @@ int main()
 === Code Execution Successful ===
 
 Enter no of lines: 5
-246810
-1214161820
-2224262830
-3234363840
-4244464850
+2 4 6 8 10 
+12 14 16 18 20 
+22 24 26 28 30 
+32 34 36 38 40 
+42 44 46 48 50 
+
 
 === Code Execution Successful ===
 */
