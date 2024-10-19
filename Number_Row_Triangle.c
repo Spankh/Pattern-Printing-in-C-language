fbb9657766bd1b381e08c1e1a6363fc04980b236
PANKHURI-3110 <110279557+Spankh@users.noreply.github.com>
2024-10-19 22:54:35 +0530

    Number Pattern - triangle with there respective row no

---
+++ Number_Row_Triangle.c
@@ -0,0 +1,35 @@
+// Number Pattern - triangle with there respective row no
+
+#include <stdio.h>
+
+int main()
+{
+    int n; // Number of lines
+    printf("Enter no of lines: ");
+    scanf(“%d”, &n); 
+    
+    for (int i = 1; i <= n; i++){ //outer loop for row
+        
+        for (int j = 1; j <= i; j++) { // inner loop for col
+         
+            printf("%d", i);
+            
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
+
+
+=== Code Execution Successful ===
+
+Enter no of lines: 5
+1
+22
+333
+4444
+55555
+
+
+=== Code Execution Successful ===
