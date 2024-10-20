569971f640a839dddb17120838ad65a6ef168cdb
PANKHURI-3110 <110279557+Spankh@users.noreply.github.com>
2024-10-20 23:47:16 +0530

    Rhombus

---
+++ Rhombus.c
@@ -0,0 +1,40 @@
+// Rhombus
+
+#include <stdio.h>
+
+int main()
+{
+    int n; // Number of lines
+    printf("Enter no of lines: ");
+    scanf(“%d”, &n); 
+    
+    for (int i = 1; i <= n; i++){ //outer loop for row
+
+        for (int j = 1; j <= n-i; j++) { // inner loop for col to print space brfore star 
+           printf(" "); // print space
+        }
+        
+        for(int k = 1; k <= n; k++){ //nested loop for col to print *
+            printf("*");
+        }
+        
+        printf("\n");
+        
+    }
+    return 0;
+}
+
+
+/*
+=== Code Execution Successful ===
+
+Enter no of lines: 4
+   ****
+  ****
+ ****
+****
+
+
+=== Code Execution Successful ===
+
+*/
